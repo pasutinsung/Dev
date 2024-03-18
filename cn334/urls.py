4672fb30e6c87c9164b7120ef88ae67b357eb424
pasutinsung <pasut.run@gmail.com>
2024-03-18 16:18:18 +0700

    update

--- cn334/urls.py
+++ cn334/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin 
+from django.urls import path, include 
+
+from ecommerce import views as ecom_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("ecommerce/", ecom_views.ecommerce_index_view),
+    path("ecommerce/item/<item_id>", ecom_views.item_view),
+    path("ecommerce/home", ecom_views.item_view),
+    path("ecommerce/category", ecom_views.item_view),
+    path("ecommerce/product", ecom_views.item_view),
+    path("ecommerce/checkout", ecom_views.item_view),
+    path("ecommerce/contact", ecom_views.item_view),
+    path("w09/request", ecom_views.basic_request)
+    ]
+
--- general/views.py
+++ general/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+def ecommerce_index_view(request):
+    '''This function render index page of ecommerce views'''
+
+    return HttpResponse('Welcome to 6410742537 Setthanan Thitathanapat views!')
+
+def item_view(request, item_id):
+
+    context_data = {
+        "item_id": item_id
+    }
+    return render(request, 'index.html',context= context_data)
+
+def item_view1(request):
+
+    return render(request, 'homepage.html')
+
+def item_view2(request):
+
+    return render(request, 'category.html')
+
+def item_view3(request):
+
+    return render(request, 'product.html')
+
+def item_view4(request):
+
+    return render(request, 'checkout.html')
+
+def item_view5(request):
+
+    return render(request, 'contact.html')
+
+@csrf_exempt
+def basic_request(requests):
+    if requests.method == "GET":
+        return JsonResponse({"status": "GET Pass"}, safe=False)
+
+    if requests.method == "POST":
+        return JsonResponse({"status": "POST Pass"}, safe=False)
+
